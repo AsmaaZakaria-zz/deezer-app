c17e76e3df4b51197db1ca3d56d5ae0068d6e98e
AsmaaZakaria <asmaazakaria94@gmail.com>
2020-01-30 23:30:11 +0200

    set heroku url

--- src/modules/user/actions/index.js
+++ src/modules/user/actions/index.js
@@ -2,11 +2,15 @@ import { history } from "./../../../history";
 import { LOG_IN_SUCCESS, LOG_IN_FAILURE, LOG_OUT } from "./../constants";
 
 const { DZ } = window;
+const URL =
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/"
+        : "https://deezer-app.herokuapp.com";
 
 DZ.init({
     appId: "392944",
     secret: "bdf0147e1fbda79d41721573c8d352c4",
-    channelUrl: "http://localhost:3000/",
+    channelUrl: URL,
 });
 
 export const userLogin = () => async (dispatch) => {
